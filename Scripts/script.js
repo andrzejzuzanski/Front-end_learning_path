f83f891c8ff09f938c67949d8e74e13d8c2e898a
Andrzej Zuzański <andrzejzuzanski@gmail.com>
2022-07-24 21:15:07 +0200

    Change color palete, added fonts
    Changed colors, added font, tweaked css and easter egg in js file

--- Scripts/script.js
+++ Scripts/script.js
@@ -1,7 +1,7 @@
+//Background color change script //
 const bodySelector = document.querySelector("body");
-
 window.addEventListener("scroll", () => {
-  const defaultColor = { red: 187, green: 208, blue: 255 };
+  const defaultColor = { red: 250, green: 237, blue: 205 };
   const divider = 1 + (window.scrollY || window.pageYOffset) / 1500;
   const [r, g, b] = [
     defaultColor.red / divider,
@@ -10,3 +10,15 @@ window.addEventListener("scroll", () => {
   ].map(Math.round);
   bodySelector.style.backgroundColor = `rgb(${r}, ${g}, ${b})`;
 });
+
+//Easter egg script
+const easterFooterSelector = document.querySelector("#easterEgg");
+let easterCounter = 0;
+easterFooterSelector.addEventListener('click', () => {
+    if(!(easterCounter === 4)){
+        easterCounter++;
+    } else {
+        window.open("https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstley", "_blank");
+        easterCounter = 0;
+    }
+})
