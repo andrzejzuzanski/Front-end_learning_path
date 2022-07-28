4c7ea4b48e3257e8c96d5ac22267d0bdc6d5c026
Andrzej Zuzański <andrzejzuzanski@gmail.com>
2022-07-28 09:11:03 +0200

    Added navbar eventListener

--- Scripts/script.js
+++ Scripts/script.js
@@ -1,5 +1,4 @@
 let isMobile = window.matchMedia("(any-pointer:coarse)").matches;
-console.log(isMobile);
 
 //Background color change script //
 const bodySelector = document.querySelector("body");
@@ -13,6 +12,35 @@ window.addEventListener("scroll", () => {
   ].map(Math.round);
   bodySelector.style.backgroundColor = `rgb(${r}, ${g}, ${b})`;
 });
+
+// Navbar
+const navbarSelector = document.querySelector(".stacked-navbar");
+
+navbarSelector.addEventListener("click", () => {
+  let navbarHeight = navbarSelector.style.height;
+  if(navbarHeight === "" || navbarHeight === "3rem"){
+    navbarSelector.style.height = "9.5rem";
+  } else {
+    navbarSelector.style.height = "3rem";
+  }
+
+})
+
+//HTML container
+if(isMobile){
+  const allContainers = document.querySelectorAll(".main-container");
+
+  for(let container of allContainers){
+    container.addEventListener("click", () => {
+      let containerHeight = container.style.maxHeight;
+      if(containerHeight === "" || containerHeight === "5rem"){
+        container.style.maxHeight = "80vh"
+      } else {
+        container.style.maxHeight = "5rem"
+      }
+  })
+  };
+}
 
 //Easter egg script
 const easterFooterSelector = document.querySelector("#easterEgg");
@@ -28,19 +56,3 @@ easterFooterSelector.addEventListener("click", () => {
     easterCounter = 0;
   }
 });
-
-//HTML container
-if(isMobile){
-  const allContainers = document.querySelectorAll(".main-container");
-
-  for(let container of allContainers){
-    container.addEventListener("click", () => {
-      let aaa = container.style.maxHeight;
-      if(aaa === "" || aaa === "5rem"){
-        container.style.maxHeight = "80vh"
-      } else {
-        container.style.maxHeight = "5rem"
-      }
-  })
-  };
-}
